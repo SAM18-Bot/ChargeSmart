c3630abf2cf678b40b50419ad0ffbe0b9a355776
Sameer Bansode <sameerbansode001@gmail.com>
2025-09-03 06:15:03 +0000

    is there any issues with permission or something wrong with authunticati

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,7 +13,7 @@ import { useToast } from '@/hooks/use-toast';
 import { Loader2 } from 'lucide-react';
 
 const GoogleIcon = () => (
-    <svg className="mr-2 h-5 w-5" viewBox="0 0 24 24">
+    <svg className="mr-2 h-5 w-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
             d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
             fill="#4285F4"
@@ -30,7 +30,6 @@ const GoogleIcon = () => (
             d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
             fill="#EA4335"
         />
-        <path d="M1 1h22v22H1z" fill="none" />
     </svg>
 );
 
@@ -128,7 +127,7 @@ export default function LoginPage() {
               />
             </div>
             <Button type="submit" className="w-full font-bold" disabled={isSubmitting}>
-              {isSubmitting && email && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+              {isSubmitting && !!email && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
               Sign In
             </Button>
           </form>
