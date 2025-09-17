288d58ea4657d460111a0fbc01791c110afbbd75
Sameer Bansode <sameerbansode001@gmail.com>
2025-09-17 06:03:58 +0000

    change image (_for element img_)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -113,7 +113,7 @@ export default function AboutPage() {
             <div className="container mx-auto px-4 flex flex-col md:flex-row items-center gap-12">
                 <div className="md:w-1/2">
                     <Image
-                        src="https://picsum.photos/seed/about/600/400"
+                        src="https://picsum.photos/seed/chargers/600/400"
                         alt="Electric car charging in a futuristic city"
                         width={600}
                         height={400}
